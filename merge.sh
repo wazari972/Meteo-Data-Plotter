1d8a2fd055f690cb5835fbd02972d114088cedaa
Kevin P <wazari972@gmail.com>
2012-11-07 19:40:50 +0100

    work again

---
+++ merge.sh
@@ -0,0 +1,2 @@
+cat Revel.csv > Grenoble.csv
+cat Montbonnot.csv | grep -v Date >> Grenoble.csv
--- plot.R
+++ plot.R
@@ -2,32 +2,51 @@
 
 meteoRevel <- read.csv(file="Revel.csv",head=TRUE,sep=";")
 meteoMque <- read.csv(file="Martinique.csv",head=TRUE,sep=";")
+meteoBonnot <- read.csv(file="Montbonnot.csv",head=TRUE,sep=";")
+meteoGrenoble <- read.csv(file="Grenoble.csv",head=TRUE,sep=";")
 
 
-plot_pluiehygro=function(pluie, hygro) {
+plot_pluie = function(pluie) {
+  pluie <- fill_gaps_zero(pluie)
+  
 	meanPluie <- mean(pluie)
-	meanHygro <- mean(hygro)
 	
 	pluie <- complete(pluie, 0, size)
-	hygro <- complete(hygro, meanHygro, size)
+  lty.o <- par("lty")
 
-	par (mar=c(2, 4, 4, 4) + 0.1)
-	barplot (pluie, space=0,  col='lightblue', ylab='Precipitations (mm)', axes=FALSE)
-	grid (nx=size, ny=6, lty=1)
+	par (mar=c(2, 4, 4, 4) + 0.1, lty=0)
+	barplot (pluie, space=0,  col='blue', ylab='Precipitations (mm)', axes=FALSE)
+  # Reset to old value
+  par(lty = lty.o)
+	abline(h=meanPluie, col="blue",lty=2)
 
-	abline(h=meanPluie, col="lightblue",lty=2)
-	axis (2, at=seq(0,max(pluie),5))
-	
-	hygroRange <- range(hygro)
-	plot.window(xlim=c(0,size), ylim=c(50,100), xaxs='i', yaxs='i')
-	axis (4)
-	mtext ('Hygro (%)', 4, line=3)
-	lines (seq(from=0.5,to=size-0.5,by=1), hygro, col="seagreen4")
-	abline(h=meanHygro, col="seagreen4",lty=2)
+	axis (2, at=seq(0, max(pluie), 5))
+  
 	box()
 }
 
-plot_temp=function(max, min) {
+plot_hygro = function(hygro) {
+  hygro <- fill_gaps_linear(hygro)
+  
+	meanHygro <- mean(hygro)
+	
+	hygro <- complete(hygro, meanHygro, size)
+  
+	hygroRange <- range(hygro)
+	
+	plot.new()
+	plot.window(xlim=c(0,size), ylim=c(min(hygroRange),100), xaxs='i', yaxs='i')
+	axis (2)
+	mtext ('Hygro (%)', 2, line=3)
+	lines (seq(from=0.5, to=size-0.5,by=1), hygro, col="seagreen4")
+	abline(h=meanHygro, col="seagreen4", lty=2)
+	box()
+}
+
+plot_temp = function(max, min) {
+  max <- fill_gaps_linear(max)
+  min <- fill_gaps_linear(min)
+  
 	tempRange <- range(max, min)
 	tempRange <- range(min(0, min(tempRange)), max(tempRange, 35))
 
@@ -42,17 +61,18 @@ plot_temp=function(max, min) {
 	plot.new()
 	plot.window(xlim=c(0,size), ylim=tempRange, xaxs='i', yaxs='i')
 	axis (2)
-	grid (nx=size, ny=6, lty=1)
 	mtext ('Temp (°C)', 2, line=3)
 	lines (seq(0.5,size-0.5,1), max, col="tomato2")
 	abline(h=meanMax, col="tomato2",lty=2)
-	lines (seq(0.5,size-0.5,1), min, col="royalblue3")
-	abline(h=meanMin, col="royalblue3",lty=2)
-	lines (seq(0.5,size-0.5,1), average, col="orchid")
+	lines (seq(0.5,size-0.5,1), min, col="royalblue")
+	abline(h=meanMin, col="royalblue",lty=2)
+	lines (seq(0.5,size-0.5,1), average, col="royalblue")
 	box()
 }
 
-plot_pression=function(pression) {
+plot_pression = function(pression) {
+  pression <- fill_gaps_linear(pression)
+  
 	pression <- increase_to(pression, 1000)
 	pressionRange <- range(max(pression)+1, min(pression)-1) 
 	meanPression <- mean(pression)
@@ -60,18 +80,20 @@ plot_pression=function(pression) {
 	pression <- complete(pression, meanPression, size)
 	plot(0, 0, xlim=c(0,size), ylim=pressionRange, xaxs='i', yaxs='i')
 	axis (2)
-	grid (nx=size, ny=6, lty=1)
 	mtext ('Pression (hPa)', 2, line=3)
 	lines (seq(0.5,size-0.5,1), pression, col="orange3")
 	abline(h=meanPression, col="orange3",lty=2)
 	box()
 }
 
-do_plot_single=function(data1) {
+do_plot_single = function(data1) {
 	size <<- length(data1$Date)
 	
-	plot_pluiehygro(data1$Pluie, data1$Hygro)
-	title("Pluie et Hygrométrie")
+	plot_pluie(data1$Pluie)
+	title("Pluie")
+	
+	plot_hygro(data1$Hygro)
+	title("Hygrometrie")
 
 	plot_temp(data1$Temp.max, data1$Temp.min)
 	title("Temperature extérieure")
@@ -82,7 +104,7 @@ do_plot_single=function(data1) {
 	title("Pression")
 }
 
-do_plot_multi=function(data1, data2) {
+do_plot_multi = function(data1, data2) {
 	size <- length(data1$Date)
 	size <<- max(size, length(data2$Date))
 	
@@ -105,10 +127,64 @@ do_plot_multi=function(data1, data2) {
 	title("Pression (Revel)")
 	plot_pression(data2$Pression)
 	title("(Martinique)")
-
 }
 
-increase_to=function(lst, value){
+fill_gaps_zero = function(lst){
+	for (i in 1:length(lst)) {
+	  if (is.na(lst[i])) {
+	    lst[i] = 0
+	  }
+	}
+	return(lst)
+}
+
+fill_gaps_linear = function(lst){
+  max <- length(lst)
+	for (i in 1:max) {
+	  #if we have an invalid value
+	  if (!is.na(lst[i])) {
+	    next
+	  }
+
+	  #pick up the prev value, or NA if there's no
+	  if (i != 1) {
+	    prev <- lst[i-1]
+	  } else {
+   	  prev <- NA
+    }	  
+	  #set j to next valid value
+	  j <- i + 1
+		while (j < max && is.na(lst[j])) {
+		  j <- j+1
+		}
+		# i == j impossible
+		
+		# if we didn't reach the end of the list,
+		if (j != max) {
+      nxt = lst[j]
+		} else {
+		  # use the last valid value
+		  nxt = prev		
+		}
+		
+		#if we had no previous value, use the next valid one
+		if (is.na(prev)) {
+		  prev = nxt
+		}
+		
+		# fill the gap
+
+		slice = (nxt-prev)/(j-i+1)
+		for (k in i:(j)) {
+		  # with linearily increasing values, between prev and nxt
+		  lst[k] = prev + slice*(k-i+1)
+		}
+	}
+	return(lst)
+}
+#fill_gaps_linear(meteoRevel$Pluie)
+
+increase_to = function(lst, value){
 	for (i in 1:length(lst)) {
 		if (lst[i] < value)
 			lst[i] <- lst[i] + value
@@ -117,14 +193,15 @@ increase_to=function(lst, value){
 	}
 	return(lst)
 }
-get_avg=function(lst1, lst2){
+get_avg = function(lst1, lst2){
 	average <- list()
 	for (t in 1:size) {
 		average <- c(average, (lst1[t] + lst1[t]) / 2)
 	}
 	return(average)
 }
-complete=function(lst, val, totalLength){
+
+complete = function(lst, val, totalLength){
 	diff <- totalLength - length(lst)
 	if (diff == 0) {
 		return(lst)
@@ -133,5 +210,7 @@ complete=function(lst, val, totalLength){
 	}
 }
 
-do_plot_single(meteoRevel)
-#do_plot_multi(meteoRevel, meteoMque)
+#do_plot_single(meteoBonnot)
+do_plot_single(meteoGrenoble)
+
+#do_plot_multi(meteoRevel, meteoBonnot)

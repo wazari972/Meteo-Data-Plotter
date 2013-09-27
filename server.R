550736dd7dcaa482280da168c21109050b051af9
Kevin P <wazari972@gmail.com>
2013-09-27 19:29:24 +0200

    switch to last version of SHINY

--- server.R
+++ server.R
@@ -3,13 +3,13 @@ source("plot.R")
 source("config.R")
 
 shinyServer(function (input, output) {
-  output$location_selector <- reactiveUI(function() {
+  output$location_selector <- renderUI({
     checkboxGroupInput(inputId = "location",
                        label = "Location:",
                        choices = ls(meteoData))
   })
   
-  output$rainthreshold <- reactiveUI(function() {
+  output$rainthreshold <- renderUI( {
     max <- environment()
     max$Rain <- 1
     
@@ -30,73 +30,73 @@ shinyServer(function (input, output) {
     return (HEIGTH*nbPlots)
   }
   
-  selectedDataset <- reactive(function() {
+  selectedDataset <- reactive({
     return (getDatasets(input$location))
   })
   
-  output$plot_rain <- reactivePlot(function() {
+  output$plot_rain <- renderPlot({
     splitPlots(selectedDataset(), function(name, data) {
       plot_pluie(name, data$Date, data$Pluie, input)
     })
   }, height=getVarHeight)
   
-  output$plot_humid <- reactivePlot(function() { 
+  output$plot_humid <- renderPlot({
     splitPlots(selectedDataset(), function(name, data) {
       plot_hygro(name, data$Date, data$Hygrometrie, input)
     })
   }, height=getVarHeight)
   
-  output$plot_minimax <- reactivePlot(function() { 
+  output$plot_minimax <- renderPlot({
     splitPlots(selectedDataset(), function(name, data) {
       plot_temp(name, data$Date, data$Temp.max, data$Temp.min,  input)
     })
   }, height=getVarHeight)
   
-  output$plot_pressure <- reactivePlot(function() {
+  output$plot_pressure <- renderPlot({
     splitPlots(selectedDataset(), function(name, data) {
       plot_pression(name, data$Date, data$Pression, input)
     })
   }, height=getVarHeight)
 
-  output$plot_computation  <- reactivePlot(function() {
+  output$plot_computation  <- renderPlot({
       splitPlots(selectedDataset(), function(name, data) {
           plot_computation(name, data$Date, data, input)
       })
   }, height=getVarHeight)
                                        
-  output$plot_summary <- reactivePlot(function() { 
+  output$plot_summary <- renderPlot({
     splitPlots(selectedDataset(), function(name, data) {
       plot_summary(name, data, input) 
     })
   }, height=getFixHeight(4))
   
   
-  output$text_rain <- reactiveText(function() {
+  output$text_rain <- renderPrint({
     iterateDataset(selectedDataset(), function(i, name, data) {
       p(strong(name)) #doesnt work this way
       print_pluie(name, data$Date, data$Pluie, input)
     })
   })
     
-  output$text_minimax <- reactiveText(function() {
+  output$text_minimax <- renderPrint({
     iterateDataset(selectedDataset(), function(i, name, data) {
       print_temp(name, data$Date, data$Temp.max, data$Temp.min,  input)
     })
   })
   
-  output$text_humid <- reactiveText(function() {
+  output$text_humid <- renderPrint( {
     iterateDataset(selectedDataset(), function(i, name, data) {
       print_hygro(name, data$Date, data$Hygrometrie, input)
     })
   })
   
-  output$text_pressure <- reactiveText(function() {
+  output$text_pressure <- renderPrint({
     iterateDataset(selectedDataset(), function(i, name, data) {
       print_pression(name, data$Date, data$Pression, input)
     })
   })
 
-  output$text_hygro_temp <- reactiveText(function() {
+  output$text_hygro_temp <- renderPrint({
     iterateDataset(selectedDataset(), function(i, name, data) {
 #      print_computation(name, data$Date, data$Pression, input)
     })
